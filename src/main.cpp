77ce12136e844c0e9f1d3e1a7c5f8b5b609f29d6
BADAndrea <36732680+BADAndrea@users.noreply.github.com>
2020-09-17 17:03:46 +0200

    pwm add

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,12 @@ OBJ: Execution of ModBus commands
 
 #define DC_PUMP_MOTOR  //enable/disable PWM control of pump (pump v2)
 //#define DC_PUMP_MOTOR_SENSE //enable/disable sensing of pump speed via hall effect sensor
-//#define DC_PUMP_POWER_MEASURE //enable/disable measuring of voltage, current, power of pump
+#define DC_PUMP_POWER_MEASURE //enable/disable measuring of voltage, current, power of pump
 
 #include <Arduino.h>
 #include <ModbusRtu.h>
 #include <AccelStepper.h>
+#include <PWM/PWM.h>
 #ifdef DC_PUMP_POWER_MEASURE
 #include <SPI.h>
 #include <Adafruit_INA219.h>
@@ -19,18 +20,19 @@ OBJ: Execution of ModBus commands
 
 //general config paramters
 constexpr int CHIP_ID = 5; //modbus ID 
-constexpr int VECTOR_LENGHT = 12;
+constexpr int VECTOR_LENGHT = 11;
 constexpr int MICROSTEPPING = 8;
 constexpr int STEPPER_LOW_CURRENT = 50; //approx 1v -> 150mA
-constexpr int STEPPER_HIGH_CURRENT = 127; //approx 2.5v -> 400mA
+constexpr int STEPPER_HIGH_CURRENT = 150; //127 approx 2.5v -> 400mA
+constexpr int PWM_frequency = 15000; //Hz
 
 //data array positions for modbus network sharing
 constexpr int STEPPER_SPEED_VECTOR_POSITION = 0;
 constexpr int STEPPER_ACCELERATION_VECTOR_POSITION = 1;
 
-constexpr int STEPPER1_STEPS_VECTOR_POSITION = 2;
-constexpr int STEPPER1_POS1_VECTOR_POSITION = 3;
-constexpr int STEPPER1_POS2_VECTOR_POSITION = 4;
+constexpr int STEPPER_STEPS_VECTOR_POSITION = 2;
+constexpr int STEPPER_POS1_VECTOR_POSITION = 3;
+constexpr int STEPPER_POS2_VECTOR_POSITION = 4;
 
 constexpr int MODBUS_IN_COUNT_VECTOR_POSITION = 5;
 constexpr int MODBUS_OUT_COUNT_VECTOR_POSITION = 6;
@@ -38,15 +40,14 @@ constexpr int MODBUS_ERROR_COUNT_VECTOR_POSITION = 7;
 
 constexpr int DC_PUMP_SPEED_POSITION = 8;
 constexpr int DC_PUMP_RPM_FROM_SENS_POSITION = 9;
-constexpr int DC_PUMP_VOLTAGE_POSITION = 10;
-constexpr int DC_PUMP_CURRENT_POSITION = 11;
+constexpr int DC_PUMP_CURRENT_POSITION = 10;
 
 //arduino pin configuration
 constexpr int U8TXENPIN = 6;
 
-constexpr int STEPPER1_CURRENT_PIN_MOVING = 3;
-constexpr int STEPPER1_DIRECTION_PIN = 8;
-constexpr int STEPPER1_STEP_PIN = 7;
+constexpr int STEPPER_CURRENT_PIN_MOVING = 3;
+constexpr int STEPPER_DIRECTION_PIN = 8;
+constexpr int STEPPER_STEP_PIN = 7;
 
 //stepper variabiles
 bool stepper_resetting = true;
@@ -71,10 +72,9 @@ unsigned int pump_speed_percent = 0;
 #endif // DC_PUMP_MOTOR
 
 #ifdef DC_PUMP_POWER_MEASURE
-float current_mA = 0;
-float loadvoltage_mV = 0;
-unsigned long last_motor_power_update = 0;
-constexpr int POWER_UPDATE_TIME = 200; //in milliseconds
+unsigned int current_mA = 0;
+unsigned long last_motor_current_update = 0;
+constexpr int CURRENT_UPDATE_TIME = 200; //in milliseconds
 
 // Define the number of samples to keep track of. The higher the number, the
 // more the readings will be smoothed, but the slower the output will respond to
@@ -86,10 +86,6 @@ int readIndex = 0;              	// the index of the current reading
 int current_readings[numReadings];	// the readings from the analog input
 int current_total = 0;            	// the running total
 int current_average = 0;     			// the average
-
-int voltage_readings[numReadings];
-int voltage_total = 0;
-int voltage_average = 0;
 #endif
 
 //data array for modbus network sharing
@@ -104,7 +100,6 @@ uint16_t au16data[VECTOR_LENGHT] = {
  0, //ERROR count
  0, //DC pump motor speed
  0, //DC pump motor speed read from sensor
- 0, //DC pump voltage
  0 //DC pump current
 };
 
@@ -117,17 +112,17 @@ uint16_t au16data[VECTOR_LENGHT] = {
  */
 
 Modbus slave(CHIP_ID, 0, U8TXENPIN);
-AccelStepper stepper(1, STEPPER1_STEP_PIN, STEPPER1_DIRECTION_PIN); //step, dir
+AccelStepper stepper(1, STEPPER_STEP_PIN, STEPPER_DIRECTION_PIN); //step, dir
 #ifdef DC_PUMP_POWER_MEASURE
 Adafruit_INA219 ina219;
 #endif
 
 void set_holding_current(int stepper_number) {
-	analogWrite(STEPPER1_CURRENT_PIN_MOVING, STEPPER_LOW_CURRENT);
+	analogWrite(STEPPER_CURRENT_PIN_MOVING, STEPPER_LOW_CURRENT);
 }
 
 void set_moving_current(int stepper_number) {
-	analogWrite(STEPPER1_CURRENT_PIN_MOVING, STEPPER_HIGH_CURRENT);
+	analogWrite(STEPPER_CURRENT_PIN_MOVING, STEPPER_HIGH_CURRENT);
 	stepper_move_command_time = millis();
 }
 
@@ -169,8 +164,8 @@ unsigned int l_2uint_int2(long long_number) { //split the long and return second
 }
 
 void update_modbus_data() {
-	au16data[STEPPER1_POS1_VECTOR_POSITION] = l_2uint_int1(stepper.currentPosition() / 8); //split the long into 2 unsigned integers
-	au16data[STEPPER1_POS2_VECTOR_POSITION] = l_2uint_int2(stepper.currentPosition());
+	au16data[STEPPER_POS1_VECTOR_POSITION] = l_2uint_int1(stepper.currentPosition() / 8); //split the long into 2 unsigned integers
+	au16data[STEPPER_POS2_VECTOR_POSITION] = l_2uint_int2(stepper.currentPosition());
 
 	#ifdef DC_PUMP_MOTOR_SENSE
 	au16data[DC_PUMP_RPM_FROM_SENS_POSITION] = rpm_out;
@@ -180,10 +175,9 @@ void update_modbus_data() {
 	au16data[MODBUS_OUT_COUNT_VECTOR_POSITION] = slave.getOutCnt();
 	au16data[MODBUS_ERROR_COUNT_VECTOR_POSITION] = slave.getErrCnt();
 
-	au16data[STEPPER1_POS1_VECTOR_POSITION] = 500 - au16data[STEPPER1_STEPS_VECTOR_POSITION];
+	au16data[STEPPER_POS1_VECTOR_POSITION] = 500 - au16data[STEPPER_STEPS_VECTOR_POSITION];
 
 	#ifdef DC_PUMP_POWER_MEASURE
-	au16data[DC_PUMP_VOLTAGE_POSITION] = voltage_average;
 	au16data[DC_PUMP_CURRENT_POSITION] = current_average;
 	#endif
 }
@@ -195,8 +189,14 @@ void addrev() {
 #endif // DC_PUMP_MOTOR_SENSE
 
 void setup() {
-	pinMode(STEPPER1_CURRENT_PIN_MOVING, OUTPUT);
-	analogWrite(STEPPER1_CURRENT_PIN_MOVING, STEPPER_LOW_CURRENT);
+	//TCCR1B = TCCR1B & B11111000 | B00000010; // for PWM frequency of 3921.16 Hz
+	//TCCR1B = TCCR1B & B11111000 | B00000001; // set timer 1 divisor to 1 for PWM frequency of 31372.55 Hz
+
+	InitTimersSafe(); 
+	SetPinFrequencySafe(DC_PUMP_SPEED_CONTROL_PIN, PWM_frequency);
+
+	pinMode(STEPPER_CURRENT_PIN_MOVING, OUTPUT);
+	analogWrite(STEPPER_CURRENT_PIN_MOVING, STEPPER_LOW_CURRENT);
 
 	slave.begin(9600); //9600 baud, 8-bits, 1-bit stop
 	reset_vapour_position();
@@ -213,7 +213,6 @@ void setup() {
 	ina219.begin();
 	for (int thisReading = 0; thisReading < numReadings; thisReading++) {
 		current_readings[thisReading] = 0;
-		voltage_readings [thisReading] = 0;
 	}
 	#endif
 }
@@ -231,10 +230,10 @@ void loop() {
 			set_stepper_SA();
 		}
 
-		if (au16data[STEPPER1_STEPS_VECTOR_POSITION] != stepper.currentPosition() && au16data[MODBUS_IN_COUNT_VECTOR_POSITION] != slave.getInCnt()) {
+		if (au16data[STEPPER_STEPS_VECTOR_POSITION] != stepper.currentPosition() && au16data[MODBUS_IN_COUNT_VECTOR_POSITION] != slave.getInCnt()) {
 			//if stepper is not where i want and just received a command, set new position to reach
 			set_moving_current(0);
-			stepper.moveTo(au16data[STEPPER1_STEPS_VECTOR_POSITION] * MICROSTEPPING);
+			stepper.moveTo(au16data[STEPPER_STEPS_VECTOR_POSITION] * MICROSTEPPING);
 		}
 	} 
 	else {
@@ -262,31 +261,26 @@ void loop() {
 	#ifdef DC_PUMP_MOTOR
 	if (pump_speed_percent != au16data[DC_PUMP_SPEED_POSITION]) {
 		pump_speed_percent = au16data[DC_PUMP_SPEED_POSITION];
-		analogWrite(map(pump_speed_percent, 0, 100, 0, 255), DC_PUMP_SPEED_CONTROL_PIN);
+		//analogWrite(DC_PUMP_SPEED_CONTROL_PIN, (int)map(pump_speed_percent, 0, 100, 0, 255));
+		pwmWrite(DC_PUMP_SPEED_CONTROL_PIN, (int)map(pump_speed_percent, 0, 100, 0, 255));
 	}
 	#endif // DC_PUMP_MOTOR
 
 	#ifdef DC_PUMP_POWER_MEASURE
-	if (millis() - last_motor_power_update > POWER_UPDATE_TIME) {
-		current_mA = (int)ina219.getCurrent_mA();
-		loadvoltage_mV = (int)ina219.getBusVoltage_V() * 1000 + ina219.getShuntVoltage_mV();
+	if (millis() - last_motor_current_update > CURRENT_UPDATE_TIME) {
+		current_mA = ina219.getCurrent_mA();
 
 		//a bit of rolling average
 		current_total -= current_readings[readIndex];
 		current_readings[readIndex] = current_mA;
 		current_total += current_readings[readIndex];
 
-		voltage_total -= voltage_readings[readIndex];
-		voltage_readings[readIndex] = loadvoltage_mV;
-		voltage_total += voltage_readings[readIndex];
-
 		readIndex = readIndex + 1;
-		if (readIndex >= numReadings) {
+		if (readIndex == numReadings) {
 			readIndex = 0;
 		}
 
 		current_average = current_total / numReadings;
-		voltage_average = voltage_total / numReadings;
 	}
 	#endif
 }
